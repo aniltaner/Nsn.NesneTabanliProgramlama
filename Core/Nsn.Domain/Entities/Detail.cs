28d046c29fce3ff0594908b56793705eb2529454
aniltaner <aniltaner07@gmail.com>
2026-05-09 20:44:44 +0300

    Update Detail.cs

--- Core/Nsn.Domain/Entities/Detail.cs
+++ Core/Nsn.Domain/Entities/Detail.cs
@@ -1,31 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Nsn.Domain.Common;
+﻿using Ntp.Domain.Common;
 
-namespace Nsn.Domain.Entities
+namespace Ntp.Domain.Entities;
+
+public class Detail : EntityBase
 {
-    public class Detail : EntityBase
+    public Detail()
     {
-        public Detail()
-        {
 
-        }
-
-        public Detail(string title, string value, int categoryId)
-        {
-            Title = title;
-            Value = value;
-            CategoryId = categoryId;
-        }
-        public required string Title { get; set; }
-
-        public string Value { get; set; }
-
-        public required int CategoryId { get; set; }
-
-        public Category Category { get; set; }
     }
+
+    public Detail(string title, string value, int categoryId)
+    {
+        Title = title;
+        Description = value;
+        CategoryId = categoryId;
+    }
+
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
 }
