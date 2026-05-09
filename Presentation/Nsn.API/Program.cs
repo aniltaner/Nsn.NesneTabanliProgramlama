7407e8c01a3ac52c90e7b525fbe642c683088843
aniltaner <aniltaner07@gmail.com>
2026-05-09 21:15:49 +0300

    Update Program.cs

--- Presentation/Nsn.API/Program.cs
+++ Presentation/Nsn.API/Program.cs
@@ -1,26 +1,47 @@
+using Nsn.Application;
+using Nsn.Application.Exceptions;
+using Nsn.Mapper;
+using Nsn.Persistance;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+// OpenAPI (Swagger yerine yeni nesil) yapılandırması
+builder.Services.AddOpenApi();
+
+var env = builder.Environment;
+
+// Appsettings yapılandırması
+builder.Configuration
+    .SetBasePath(env.ContentRootPath)
+    .AddJsonFile("appsettings.json", optional: false)
+    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+// Senin projendeki servis kayıtları (Extension metotlar üzerinden)
+builder.Services.AddPersistance(builder.Configuration);
+builder.Services.AddApplication();
+builder.Services.AddCustomMapper();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// HTTP request pipeline yapılandırması
 if (app.Environment.IsDevelopment())
 {
-    //app.MapOpenApi();
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.MapOpenApi();
 }
 
-app.MapScalarApiReference(options => 
-options.Title = "Nesne Programlama Projesi");
+// SwaggerUI yerine hocanın kullandığı Scalar arayüzü
+app.MapScalarApiReference(options =>
+{
+    options.Title = "Nesne Programlama Projesi";
+    options.Theme = ScalarTheme.Default;
+});
+
+// Hocanın yazdığı hata yakalama middleware'i
+app.ConfigureExceptionHandlingMiddleware();
 
 app.UseHttpsRedirection();
 
